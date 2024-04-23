b67f0dbcd8f7ea9cfa643c2d8f243bbf287bb2f1
AaseeshSreemaan <164370119+AaseeshSreemaan@users.noreply.github.com>
2024-04-23 21:26:44 +0530

    Add files via upload

---
+++ Fruits and Vegetables/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".typing", {
+    strings: ["Apple", "Bannana", "Strawberry", "GreenGrapes", "Orange", "Watermelon"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
+
